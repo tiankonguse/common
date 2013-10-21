f5bef8eb253aff2099a6c67a057664068bb37b23
tiankonguse <i@tiankonguse.com>
2013-10-21 12:38:22 +0800

    update

--- inc/common.php
+++ inc/common.php
@@ -1,33 +1,30 @@
 <?php
-if(!defined("COMMON_LOAD")){
-	//BASE work in server
-	//DOMAIN work in client
-
-	!defined("COMMON_LOAD") && define("COMMON_LOAD", true);
-	!defined("BASE") && define("BASE",dirname(dirname(dirname(__FILE__)))."/");
-
-	if(strcmp("/home/tiankonguse/public_html/", BASE) !=0){
-		!defined("DOMAIN") && define("DOMAIN", "http://tiankonguse.com");
-	}else{
-		!defined("DOMAIN") && define("DOMAIN", "http://tk.com:8080");
-	}
-
-	!defined("BASE_COMMON") && define("BASE_COMMON", BASE."common/");
-	!defined("BASE_INC") && define("BASE_INC", BASE_COMMON."inc/");
-
-	!defined("DOMAIN_COMMON") && define("DOMAIN_COMMON", DOMAIN."/common/");
-	!defined("DOMAIN_IMG") && define("DOMAIN_IMG", DOMAIN_COMMON."img/");
-	!defined("DOMAIN_JS") && define("DOMAIN_JS", DOMAIN_COMMON."js/");
-	!defined("DOMAIN_CSS") && define("DOMAIN_CSS", DOMAIN_COMMON."css/");
-	!defined("DOMAIN_kindeditor") && define("DOMAIN_kindeditor", DOMAIN_COMMON."kindeditor/");
-	!defined("DOMAIN_datepicker") && define("DOMAIN_datepicker", DOMAIN_COMMON."datepicker/");
-
-
-	!defined("DOMAIN_HOME") && define("DOMAIN_HOME", DOMAIN."/i/");
-	!defined("DOMAIN_RECORD") && define("DOMAIN_RECORD", DOMAIN."/record/");
-	!defined("DOMAIN_RJBQ") && define("DOMAIN_RJBQ", DOMAIN."/rjbq/");
-	!defined("DOMAIN_LOG") && define("DOMAIN_LOG", DOMAIN."/log/");
-	!defined("DOMAIN_TIMELINE") && define("DOMAIN_TIMELINE", DOMAIN."/timeline/");
-
+if (! defined ( "COMMON_LOAD" )) {
+    // BASE work in server
+    // DOMAIN work in client
+    
+    ! defined ( "COMMON_LOAD" ) && define ( "COMMON_LOAD", true );
+    ! defined ( "BASE" ) && define ( "BASE", dirname ( dirname ( dirname ( __FILE__ ) ) ) . "/" );
+    
+    
+    ! defined ( "_DOMAIN" ) && define ( "_DOMAIN", "tk.com:8080" );
+    ! defined ( "DOMAIN" ) && define ( "DOMAIN", "http://"._DOMAIN );
+    
+    ! defined ( "BASE_COMMON" ) && define ( "BASE_COMMON", BASE . "common/" );
+    ! defined ( "BASE_INC" ) && define ( "BASE_INC", BASE_COMMON . "inc/" );
+    
+    ! defined ( "DOMAIN_COMMON" ) && define ( "DOMAIN_COMMON", DOMAIN . "/common/" );
+    ! defined ( "DOMAIN_IMG" ) && define ( "DOMAIN_IMG", DOMAIN_COMMON . "img/" );
+    ! defined ( "DOMAIN_JS" ) && define ( "DOMAIN_JS", DOMAIN_COMMON . "js/" );
+    ! defined ( "DOMAIN_CSS" ) && define ( "DOMAIN_CSS", DOMAIN_COMMON . "css/" );
+    ! defined ( "DOMAIN_kindeditor" ) && define ( "DOMAIN_kindeditor", DOMAIN_COMMON . "kindeditor/" );
+    ! defined ( "DOMAIN_datepicker" ) && define ( "DOMAIN_datepicker", DOMAIN_COMMON . "datepicker/" );
+    
+    ! defined ( "DOMAIN_HOME" ) && define ( "DOMAIN_HOME", DOMAIN . "/i/" );
+    ! defined ( "DOMAIN_RECORD" ) && define ( "DOMAIN_RECORD", DOMAIN . "/record/" );
+    ! defined ( "DOMAIN_RJBQ" ) && define ( "DOMAIN_RJBQ", DOMAIN . "/rjbq/" );
+    ! defined ( "DOMAIN_LOG" ) && define ( "DOMAIN_LOG", DOMAIN . "/log/" );
+    ! defined ( "DOMAIN_TIMELINE" ) && define ( "DOMAIN_TIMELINE", DOMAIN . "/timeline/" );
+    ! defined ( "DOMAIN_LAB" ) && define ( "DOMAIN_LAB", DOMAIN . "/lab/" );
 }
 ?>
--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,40 +1,39 @@
 <div class="_message modal-backdrop hide"></div>
 <div id="_message" class="modal hide modal-transition">
-    <div class="modal-header">
-        <button type="button" class="close">×</button>
-        <h3 id="messageModalLabel">warnning</h3>
-    </div>
-    <div class="modal-body">
-        <p></p>
-    </div>
-    <div class="modal-footer">
-        <button class="btn ok">确定</button>
-    </div>
+	<div class="modal-header">
+		<button type="button" class="close">×</button>
+		<h3 id="messageModalLabel">warnning</h3>
+	</div>
+	<div class="modal-body">
+		<p></p>
+	</div>
+	<div class="modal-footer">
+		<button class="btn ok">确定</button>
+	</div>
 
 </div>
 
 <div class="footer">
-    <div class="copyright">
-        Copyright © 2012 ~
-        <script>document.write(new Date().getFullYear());</script>
-        <a href="<?php echo DOMAIN?>">tiankonguse.com</a>. All rights
-        reserved.
-    </div>
-    <div class="copyright">联系邮箱：i@tiankonguse.com</div>
-    <div class="copyright">QQ : 804345178</div>
-    <div class="copyright">如果本站侵犯你的权利，请联系上面邮箱，我会会尽快删除.</div>
-    <div class="copyright">
-        <script type="text/javascript"
-            src="http://tajs.qq.com/stats?sId=26608498" charset="UTF-8"></script>
-        <script type="text/javascript">
+	<div class="copyright">
+		Copyright © 2012 ~
+		<script>document.write(new Date().getFullYear());</script>
+		<a href="<?php echo DOMAIN?>">tiankonguse.com</a>. All rights
+		reserved.
+	</div>
+	<div class="copyright">联系邮箱：i@tiankonguse.com</div>
+	<div class="copyright">QQ : 804345178</div>
+	<div class="copyright">如果本站侵犯你的权利，请联系上面邮箱，我会会尽快删除.</div>
+	<div class="copyright">
+		<script type="text/javascript"
+			src="http://tajs.qq.com/stats?sId=26608498" charset="UTF-8"></script>
+		<script type="text/javascript">
 			var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-			document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F7c743a9badeff72a194b85235f301f37' type='text/javascript'%3E%3C/script%3E"));
+			document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F7c743a9badeff72a194b85235f301f37' type='text/javascript'  %3E%3C/script%3E"));
         </script>
-        <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1000110679'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1000110679%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
-    </div>
+		<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1000110679'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1000110679%26show%3Dpic' type='text/javascript' %3E%3C/script%3E"));</script>
+	</div>
 </div>
-<div
-    class="top-btn top-show top-hide"></div>
+<div class="top-btn top-show top-hide"></div>
 <script>
 function showMessage(message) {
     var $message = $("#_message");
