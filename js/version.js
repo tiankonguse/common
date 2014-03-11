363bbe47d1a219511218c0a77c3fe4e879e802ea
tiankonguse <i@tiankonguse.com>
2014-03-11 22:23:06 +0800

    update loader

--- js/version.js
+++ js/version.js
@@ -1,5 +1,9 @@
-(function(loader) {
+(function(TK) {
     "use strict";
+    var loader = TK.loader = TK.loader || {};
+
+    loader.version = loader.version || "1.04";
+    loader.load = false;
     var versionList = [];
     versionList["/common/js/main.js"] = "1.01";
     versionList["/i/css/main.css"] = "1.01";
@@ -8,6 +12,6 @@
     versionList["/record/js/write.js"] = "1.04";
     versionList["/common/kindeditor/plugins/code/prettify.css"] = "1.02.2";
     loader.getVersion = function(url) {
-	return versionList[url];
+        return versionList[url];
     };
-})(TK.loader);
+})(TK);
