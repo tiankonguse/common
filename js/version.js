52f35090f47b7f57402b96642fc2a15053551d6a
tiankonguse <i@tiankonguse.com>
2014-10-24 19:37:59 +0800

    update

--- js/version.js
+++ js/version.js
@@ -8,7 +8,7 @@
     versionList["/common/js/main.js"] = "1.01";
     versionList["/i/css/main.css"] = "1.01";
     versionList["/common/css/main.css"] = "1.07";
-    versionList["/record/css/main.css"] = "1.04.36";
+    versionList["/record/css/main.css"] = "1.04.40";
     versionList["/record/js/main.js"] = "1.04.02";
     versionList["/record/js/write.js"] = "1.07";
     versionList["/common/kindeditor/plugins/code/prettify.css"] = "1.02.2";
