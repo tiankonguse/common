73ace03ab108deb620786e213b7cfbab3a7b0984
tiankonguse <i@tiankonguse.com>
2016-06-11 08:16:24 +0000

    fix mysql

---
+++ inc/fix_mysql.php
@@ -0,0 +1,92 @@
+<?php
+
+if(!function_exists("mysql_connect")){
+    function mysql_connect($server, $username, $password){
+        global $conn;
+        return mysqli_connect($server, $username, $password);
+    }
+}
+
+function db_function_help($oldFun, $newFun){
+    if(!function_exists($oldFun)){
+        $commonHead = 'global $conn;if(!$link){$link=$conn;} return ' . $newFun .'($link, $param);';
+        return create_function('$param,$link=false', $commonHead);
+    }else{
+        return $oldFun;
+    }
+}
+
+if(!function_exists("mysql_query")){
+    function mysql_query($sql, $link = false){
+        global $conn;
+        if(!$link){
+            $link = $conn;
+        }
+        return mysqli_query($link, $sql); 
+    }
+}
+
+if(!function_exists("mysql_select_db")){
+    function mysql_select_db($dbname, $link = false){
+        global $conn;
+        if(!$link){
+            $link = $conn;
+        }
+        return mysqli_select_db($link, $dbname);
+
+    }
+}
+
+if(!function_exists("mysql_real_escape_string")){
+    function mysql_real_escape_string($unescape_string, $link = false){
+        global $conn;
+        if(!$link){
+            $link = $conn;
+        }
+        return mysqli_real_escape_string($link, $unescape_string);
+    }
+}
+
+
+if(!function_exists("mysql_fetch_array")){
+    function mysql_fetch_array($result, $link = false){
+        global $conn;
+        if(!$link){
+            $link = $conn;
+        }
+        return mysqli_fetch_array($result);
+    }
+}
+
+if(!function_exists("mysql_num_rows")){
+    function mysql_num_rows($result, $link = false){
+        global $conn;
+        if(!$link){
+            $link = $conn;
+        }
+        return mysqli_num_rows($result);
+    }
+}
+
+
+if(!function_exists("mysql_insert_id")){
+    function mysql_insert_id($link = false){
+        global $conn;
+        if(!$link){
+            $link = $conn;
+        }
+        return mysql_insert_id($link);
+    }
+}
+
+
+if(!function_exists("mysql_close")){
+    function mysql_close($link = false){
+        global $conn;
+        if(!$link){
+            $link = $conn;
+        }
+        return mysql_close($link);
+    }
+}
+
--- inc/init.php
+++ inc/init.php
@@ -1,5 +1,6 @@
 <?php
 
+
 ini_set('date.timezone','Asia/Shanghai');
 date_default_timezone_set('Asia/Shanghai');
 
@@ -15,29 +16,31 @@ define('SALT','tiankonguse');
 $conn = false;
 $result = false;
 
-//showError();
+showError();
 
 function showError(){
     ini_set("display_errors","1");
     ini_set("error_reporting",E_ALL);
 }
 
-$ret = connectDB();
+include(dirname(__FILE__) . "/fix_mysql.php");
 
+
+$ret = connectDB();
 function connectDB(){
     global $conn;
     global $result;
 
     //连接mysql
-    $conn = mysqli_connect(DB_HOST,DB_USER,DB_PASS);
+    $conn = mysql_connect(DB_HOST,DB_USER,DB_PASS);
     if(!$conn)return (output(1,"连接mysql失败,请联系管理员."));
 
     //设置编码为utf8
-    $result = mysqli_query("set names utf8");
+    $result = mysql_query("set names utf8");
     if(!$result)return (output(2,"设置编码为utf8失败,请联系管理员."));
 
     //设置数据库
-    $result = mysqli_select_db(DB_NAME);
+    $result = mysql_select_db(DB_NAME);
     if(!$result)return (output(3,"选择数据库失败,请联系管理员."));
 
     return false;
